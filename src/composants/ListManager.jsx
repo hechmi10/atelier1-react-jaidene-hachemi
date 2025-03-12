93e4ba5b9b9d1cbe011750a19124c21882a9fec6
Jaidene Hachemi <hechmi.jaidane@gmail.com>
2025-03-12 08:37:08 +0100

    Mise en forme

--- src/composants/ListManager.jsx
+++ src/composants/ListManager.jsx
@@ -1,43 +1,43 @@
-import{useState} from 'react';
+import { useState } from 'react';
 import PropTypes from 'prop-types'
 
-function ListManager(props){
-  const {initialItems,placeholder} = props;
-  const [items,setItems]=useState(initialItems);
+function ListManager(props) {
+  const { initialItems, placeholder } = props;
+  const [items, setItems] = useState(initialItems);
   const [inputValue, setInputValue] = useState("");
   const add = () => {
     if (inputValue.trim() === "") return;
     setItems([...items, inputValue]);
     setInputValue("");
-};
-const handleInputChange = (e) => {
+  };
+  const handleInputChange = (e) => {
     setInputValue(e.target.value);
-};
-const supprimer=(index)=>{
-    setItems(items.filter((_,i)=>i!==index));
-}
-  return(
+  };
+  const supprimer = (index) => {
+    setItems(items.filter((_, i) => i !== index));
+  }
+  return (
     <>
-     <h1>Liste:</h1>
-     <ul>
+      <h1>Liste:</h1>
+      <ul>
         {items.map((item, index) => (
-                    
-                    <li key={index}>
-                        {item}
-                        <button onClick={()=>supprimer(index)}>Supprimer</button>
-                    </li>
-                    
-                ))}
-    </ul>
-     <input type="text" placeholder={placeholder} value={inputValue} onChange={handleInputChange}/>
-     <button onClick={add}>Ajouter</button>
+
+          <li key={index}>
+            {item}
+            <button onClick={() => supprimer(index)}>Supprimer</button>
+          </li>
+
+        ))}
+      </ul>
+      <input type="text" placeholder={placeholder} value={inputValue} onChange={handleInputChange} />
+      <button onClick={add}>Ajouter</button>
     </>
   )
 }
 
-ListManager.propTypes={
-    initialItems:PropTypes.array.isRequired,
-    placeholder:PropTypes.string.isRequired
+ListManager.propTypes = {
+  initialItems: PropTypes.array.isRequired,
+  placeholder: PropTypes.string.isRequired
 }
 
 export default ListManager;
